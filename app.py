b424e08454eec0b04597106180c8c81ae878fb22
Jedidiah Oladele <jehddie@gmail.com>
2024-12-28 20:36:51 +0100

    updated requirements

--- app.py
+++ app.py
@@ -1,3 +1,20 @@
+import requests
+
+
+def get_external_ip():
+    response = requests.get("https://api64.ipify.org?format=json")
+    if response.status_code == 200:
+        data = response.json()
+        return data.get("ip")
+    else:
+        return "Unknown"
+
+
+external_ip = get_external_ip()
+import streamlit as st
+
+st.write("External IP:", external_ip)
+
 from src.streamlit_app import StreamlitApp
 
 
