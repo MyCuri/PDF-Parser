fbc449b314847caf2b2fce9851b8073f1599168c
Jedidiah Oladele <jehddie@gmail.com>
2024-12-28 20:40:18 +0100

    removed log

--- app.py
+++ app.py
@@ -1,20 +1,3 @@
-import requests
-
-
-def get_external_ip():
-    response = requests.get("https://api64.ipify.org?format=json")
-    if response.status_code == 200:
-        data = response.json()
-        return data.get("ip")
-    else:
-        return "Unknown"
-
-
-external_ip = get_external_ip()
-import streamlit as st
-
-st.write("External IP:", external_ip)
-
 from src.streamlit_app import StreamlitApp
 
 
